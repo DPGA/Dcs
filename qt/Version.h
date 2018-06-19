940f5d109f1d09b030eb1a3afa767a40905b7123
dalambert <daniel.lambert@clermont.in2p3.fr>
2018-06-19 14:33:29 +0200

    add git ver in dial about

--- qt/Version.h
+++ qt/Version.h
@@ -5,6 +5,6 @@
  * These values are automatically set according to their cmake variables.
  */
 #define __DCS_VERSION_STRING__ "1.0.002"
-#define __GITVER__  "d6c9094"
+#define __GITVER__  "da192aa"
 
 #endif /* EXAMPLE_H */
--- qt/dial_about.cpp
+++ qt/dial_about.cpp
@@ -13,6 +13,7 @@
 #include "dial_about.h"
 #include "ui_dial_about.h"
 #include "Version.h"
+#include <QDate>
 
 //===============================================
 Dial_About::Dial_About(QWidget *parent) :
@@ -25,7 +26,8 @@ Dial_About::Dial_About(QWidget *parent) :
 
     ui->Version->setStyleSheet("QLabel { background-color : red; color : blue; } ");
 //	ui->label_Version->setText(" Experimental Version : 2.00");
-    ui->Version->setText("Version DCS " + QString(__DCS_VERSION_STRING__)+ "  Git " + QString(__GITVER__) + "\n" + QString(__DATE__) +"\t" + QString(__TIME__));
+    ui->Version->setText("Version DCS " + QString(__DCS_VERSION_STRING__)+ "  Git " + QString(__GITVER__) + "\n" + 
+		QString(__DATE__) + " " + QString(__TIME__));
 	QPixmap image (":images/catfootprint.png");
 	ui->label_img->setPixmap(image);
 }
