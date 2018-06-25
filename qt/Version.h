5130c65e067af83e708fb0536fdfe9f8a6360114
Daniel Christophe Lambert <lambert@clermont.in2p3.fr>
2018-06-25 10:07:52 +0200

    add .gitignore

--- qt/Version.h
+++ qt/Version.h
@@ -5,6 +5,6 @@
  * These values are automatically set according to their cmake variables.
  */
 #define __DCS_VERSION_STRING__ "1.0.002"
-#define __GITVER__  "f948ebb"
+#define __GITVER__  "f3729f5"
 
 #endif /* EXAMPLE_H */
