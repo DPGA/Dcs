e53ddfe03981fa54cf19a59369c52a07f2b58305
Daniel Christophe Lambert <lambert@clermont.in2p3.fr>
2019-01-07 09:07:48 +0100

    first commit 2019

--- qt/Version.h
+++ qt/Version.h
@@ -5,6 +5,6 @@
  * These values are automatically set according to their cmake variables.
  */
 #define __DCS_VERSION_STRING__ "1.0.002"
-#define __GITVER__  "336ed22"
+#define __GITVER__  "fae1ad4"
 
 #endif /* EXAMPLE_H */
